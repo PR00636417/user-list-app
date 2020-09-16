f1134737ad91bedd12762d86384752ed1a52c1be
Potta Bathini Roja <PR00636417@techmahindra.com>
2020-09-16 21:15:47 +0530

    Updates

---
+++ precache-manifest.44ed41bc592308d9287763168ef6a31e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d796d2db4a35b39908acbd21225f22",
+    "url": "/user-list-app/index.html"
+  },
+  {
+    "revision": "e4f312862fb86c9b5e14",
+    "url": "/user-list-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6af4bac5b4dded13c96",
+    "url": "/user-list-app/static/css/main.d762227e.chunk.css"
+  },
+  {
+    "revision": "e4f312862fb86c9b5e14",
+    "url": "/user-list-app/static/js/2.d8224c55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/user-list-app/static/js/2.d8224c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6af4bac5b4dded13c96",
+    "url": "/user-list-app/static/js/main.c6843701.chunk.js"
+  },
+  {
+    "revision": "c9cbb1809cbb1774705f",
+    "url": "/user-list-app/static/js/runtime-main.626103d4.js"
+  },
+  {
+    "revision": "46b3d88f94190b8a267640db5bb07e26",
+    "url": "/user-list-app/static/media/left-arrow.46b3d88f.svg"
+  },
+  {
+    "revision": "1dc1d55ff6a0fdd910b72bea5b3c9b6e",
+    "url": "/user-list-app/static/media/right-arrow.1dc1d55f.svg"
+  }
+]);
